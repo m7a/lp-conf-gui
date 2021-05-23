58608af7e0d0ff3d157f25a7dbe49e22fff76229
Linux-Fan, Ma_Sys.ma <Ma_Sys.ma@web.de>
2021-05-23 23:18:37 +0200

    Switch to new mdvl-infobg package for background images.

--- mdvl-gui.d/50-infobg.sh
+++ mdvl-gui.d/50-infobg.sh
@@ -3,6 +3,7 @@
 if [ -f "$HOME/.mdvl/infobg/bginfo.pid" ]; then
 	rm "$HOME/.mdvl/infobg/bginfo.pid"
 fi
-if [ -x "$HOME/.mdvl/user_bin/ma_infobg" ]; then
-	"$HOME/.mdvl/user_bin/ma_infobg" -i -a || true # no fail if absent
+
+if [ -x /usr/bin/mainfobg3.pl ]; then
+	/usr/bin/mainfobg3.pl
 fi
